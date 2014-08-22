1a900fe199f47c55521561dc67356228af8fc759
Saad Mahmood <smahmoo4@hawk.iit.edu>
2014-08-22 01:52:41 -0500

    Update spiralPrint.java

--- spiralPrint.java
+++ spiralPrint.java
@@ -1,3 +1,6 @@
+// spiral print numbers in 2d array so if below is the array, result should be:
+// 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 
+
 class SpiralPrint {
     public static void main(String[] args) {
         int [][] dubArr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
